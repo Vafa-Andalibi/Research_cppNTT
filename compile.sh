bbeee54c3598409bf0090a4c770406342e956ab8
Jack Langston <jclangst@indiana.edu>
2016-11-23 12:43:14 -0500

    Big Red Attempt 3 - Trying to get MPI to work 3

---
+++ compile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#This is for Big Red II compilation
+mpicxx -std=c++11 -Ofast -fopenmp -o ../search src/GameNode.cpp src/GameNode.h src/main.cpp src/MoveTree.cpp src/MoveTree.h src/MiniMax.cpp src/MiniMax.h src/UI.cpp src/UI.h -I../local/lib/bdb -lboost_system -lboost_filesystem -L../local/lib/bdb -ldb_cxx
