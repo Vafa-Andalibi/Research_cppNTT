76a2e5047a09aab28a4608d4f328f5c3aee48d43
Jack Langston <jclangst@indiana.edu>
2016-11-23 13:03:16 -0500

    Big Red Attempt 3 - Launch Command

--- compile.sh
+++ compile.sh
@@ -5,4 +5,4 @@ module unload PrgEnv-cray
 module load PrgEnv-gnu
 module load openmpi
 module load boost
-mpicxx -cxx=CC -std=c++11 -Ofast -fopenmp -o ../search src/GameNode.cpp src/GameNode.h src/main.cpp src/MoveTree.cpp src/MoveTree.h src/MiniMax.cpp src/MiniMax.h src/UI.cpp src/UI.h -I../local/lib/bdb -lboost_system -lboost_filesystem -L../local/lib/bdb -ldb_cxx
+mpiCC -std=c++11 -Ofast -fopenmp -o ../search src/GameNode.cpp src/GameNode.h src/main.cpp src/MoveTree.cpp src/MoveTree.h src/MiniMax.cpp src/MiniMax.h src/UI.cpp src/UI.h -I../local/lib/bdb -lboost_system -lboost_filesystem -L../local/lib/bdb -ldb_cxx
