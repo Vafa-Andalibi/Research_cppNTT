4b8465aa48b982245a9b239952d4d01ccd9238fe
Jack Langston <jclangst@indiana.edu>
2017-01-16 10:34:05 -0500

    Run Test 1

--- aprun.sh
+++ aprun.sh
@@ -4,4 +4,4 @@
 #PBS -N notakto
 #PBS -q debug_cpu
 
-aprun -n 4 -N 4 -d 8 search -ram 7 -threads 7 -n 5 -dir test/
+aprun -n 4 -N 4 -d 8 search -ram 7 -threads 7 -n 5 -dir /N/u/jclangst/BigRed2/test
