6c9dc39c91b0883d1822d3028be591553066fea7
Jack Langston <jclangst@indiana.edu>
2017-01-16 10:30:02 -0500

    Cluster Compile V4

--- ccmrunCompile.sh
+++ ccmrunCompile.sh
@@ -7,4 +7,4 @@
 #PBS -l gres=ccm
 #PBS -V
 
-ccmrun mpicxx -std=c++11 -Ofast -fopenmp -o ~/search ~/cppNTT/src/GameNode.cpp ~/cppNTT/src/main.cpp ~/cppNTT/src/MoveTree.cpp ~/cppNTT/src/MiniMax.cpp ~/cppNTT/src/UI.cpp -I~/local/lib/bdb -lboost_system -lboost_filesystem -L~/local/lib/bdb -ldb_cxx -I/N/soft/cle4/openmpi/gnu/1.8.4/include -pthread -Wl,-rpath -Wl,/N/soft/cle4/openmpi/gnu/1.8.4/lib -Wl,--enable-new-dtags -L/N/soft/cle4/openmpi/gnu/1.8.4/lib -lmpi_cxx -lmpi
+ccmrun mpicxx -std=c++11 -Ofast -fopenmp -o ~/search ~/cppNTT/src/GameNode.cpp ~/cppNTT/src/main.cpp ~/cppNTT/src/MoveTree.cpp ~/cppNTT/src/MiniMax.cpp ~/cppNTT/src/UI.cpp -I/N/u/jclangst/BigRed2/local/lib/bdb -lboost_system -lboost_filesystem -L/N/u/jclangst/BigRed2/local/lib/bdb -ldb_cxx -I/N/soft/cle4/openmpi/gnu/1.8.4/include -pthread -Wl,-rpath -Wl,/N/soft/cle4/openmpi/gnu/1.8.4/lib -Wl,--enable-new-dtags -L/N/soft/cle4/openmpi/gnu/1.8.4/lib -lmpi_cxx -lmpi
